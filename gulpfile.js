bafdd56079e6e48eeaac2d76b56df07aec5b5836
Tarek Sadek <t2madrid@gmail.com>
2018-05-30 19:11:28 +0200

    copy html

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var autoprefixer = require('gulp-autoprefixer');
 
+//paths
 var SOURCEPATHS = {
-		sassSource: 'src/scss/*.scss'
+		sassSource: 'src/scss/*.scss',
+		htmlSource: 'src/html/*.html'
 }
 
 var APPPATH = {
@@ -13,12 +16,21 @@ var APPPATH = {
 		js: 'app/js'
 }
 
+//sass compiler
 gulp.task('sass', function(){
 	return gulp.src('src/scss/app.scss')
+		.pipe(autoprefixer('last 2 versions')) //adding cross browser css (-webkit-)
 		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
 		.pipe(gulp.dest(APPPATH.css));
 });
 
+//copy HTML files to app
+gulp.task('copyHTML', function(){
+	gulp.src(SOURCEPATHS.htmlSource)
+		.pipe(gulp.dest(APPPATH.root));
+});
+
+//browserSync
 gulp.task('serve', ['sass'], function(){
 	browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
 		server: {
@@ -27,8 +39,10 @@ gulp.task('serve', ['sass'], function(){
 	})
 });
 
-gulp.task('watch', ['serve', 'sass'], function(){
+//watch for changes
+gulp.task('watch', ['serve', 'sass', 'copyHTML'], function(){
 	gulp.watch([SOURCEPATHS.sassSource], ['sass']);
+	gulp.watch([SOURCEPATHS.htmlSource], ['copyHTML']);
 });
 
 gulp.task('default', ['watch']);
